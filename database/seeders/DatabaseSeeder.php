da6e3c175fc40d889d4494f2a483bcc38fc5c88a
Pobitrodeb <pobitrodn@gmail.com>
2022-12-09 01:32:11 +0600

    RolepermissionSeeder, seed file create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
+use Database\Seeders\RolePermissionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
+        $this->call(RolePermissionSeeder::class);
+
     }
 }
--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 class RolePermissionSeeder extends Seeder
 {
     /**
@@ -13,6 +14,54 @@ class RolePermissionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //create role
+        $roleSupperAdmin = Role::create(['name' => 'superadmin']);
+        $roleAdmin       = Role::create(['name' => 'admin']);
+        $roleEditor      = Role::create(['name' => 'editor']);
+        $roleUser        = Role::create(['name' => 'user']);
+
+
+
+
+        //permisson roles
+        $permissions = [
+            //Dashboar
+            'dashboard.view',
+
+            //Blog permissions
+            'blog.create',
+            'blog.view',
+            'blog.edit',
+            'blog.delete',
+            'blog.approve',
+
+             //Admin permissions
+             'admin.create',
+             'admin.view',
+             'admin.edit',
+             'admin.delete',
+             'admin.approve',
+
+             //Role permissions
+             'role.create',
+             'role.view',
+             'role.edit',
+             'role.delete',
+             'role.approve',
+
+             //Profile permissions
+             'profile.view',
+             'profile.edit',
+
+        ];
+
+        //Create and Sssing permissions
+
+        for($i = 0; $i < count($permissions); $i++){
+            //create permission
+            $permission = Permission::create(['name' => $permissions[$i]]);
+            $roleSupperAdmin->givePermissionTo($permission);
+            $permission->assignRole($roleSupperAdmin);
+        }
     }
 }
