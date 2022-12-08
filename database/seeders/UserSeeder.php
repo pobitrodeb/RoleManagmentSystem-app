93892b81d82e52c68ab0c3ce960fa2e91ebfe015
Pobitrodeb <pobitrodn@gmail.com>
2022-12-09 01:00:10 +0600

    user seeder complete

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::where('email', 'pobitrodn@gmail.com')->first();
+        if(is_null($user))
+        {
+            $user           = new User();
+            $user->name     = 'Pobitro Debnath';
+            $user->email    = 'pobitrodn@gmail.com';
+            $user->password = Hash::make('12345678');
+            $user->save();
+        }
     }
 }
